eac0671bbb35f784c006f10ad8030387d049f474
Sierra McCahon <smitche4@mail.umw.edu>
2018-04-01 01:44:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
-def prime?(number)
-  if number >= 2
-    (2..number - 2).all? do |x|
-      number % x != 0
+def prime?(integer)
+  if integer >= 2
+    (2.. - 2).all? do |x|
+      integer % x != 0
     end
   else
     return false
